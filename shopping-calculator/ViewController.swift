0d89499d5bfcab0f80f9e5407a5f2b3e9a5063bd
Ege Cavusoglu <egecavusoglu99@gmail.com>
2020-08-29 20:53:37 +0300

    Initial Commit

---
+++ shopping-calculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  shopping-calculator
+//
+//  Created by Ege Çavuşoğlu on 8/29/20.
+//  Copyright © 2020 Ege Çavuşoğlu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
