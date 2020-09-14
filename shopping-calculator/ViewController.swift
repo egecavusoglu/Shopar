30dc1b1e3368a5ba348369b7988d3aeab0cbc377
Ege Cavusoglu <egecavusoglu99@gmail.com>
2020-09-14 17:55:25 +0300

    Layout added.

--- shopping-calculator/ViewController.swift
+++ shopping-calculator/ViewController.swift
@@ -10,12 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var originalPrice: UITextField!
+    @IBOutlet weak var discount: UITextField!
+    @IBOutlet weak var tax: UITextField!
+    @IBOutlet weak var finalPrice: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
     }
-
-
+    
+    @IBAction func startedChanging(_ sender: Any) {
+        print("STARTED PRINTING")
+    }
+    
+    @IBAction func handlePrice(_ sender: Any) {
+        print("PRICE", originalPrice.text! )
+        print("DISCOUNT", discount.text! )
+        print("TAX", tax.text! )
+        var oPrice =
+    }
+    
 }
 
